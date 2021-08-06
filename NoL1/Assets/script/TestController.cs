1336fd776e8540e0d6f7e23d3ca6e04e09fa4043
iipyon <kyota99930@yahoo.co.jp>
2021-08-07 00:22:33 +0900

    Unity追加コミット

---
+++ NoL1/Assets/script/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move=Vector3.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x = -moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x = moveSpeed;
+        }
+        transform.Translate(move);
+    }
+}
