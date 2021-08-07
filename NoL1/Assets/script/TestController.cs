83dc6f61e25ca609fda78c1530fc49894f380c0e
iipyon <iipyon0930@gmail.com>
2021-08-07 13:40:10 +0900

    追跡をやめる

--- NoL1/Assets/script/TestController.cs
+++ NoL1/Assets/script/TestController.cs
@@ -8,7 +8,7 @@ public class TestController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        moveSpeed = 0.5f;
     }
 
     // Update is called once per frame
