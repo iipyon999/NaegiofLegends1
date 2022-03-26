8d81e8614a791e8e1e23bb889d6e0d9b1f4a4b49
johnnysuzuki <amirak2501@gmail.com>
2022-03-26 23:15:30 +0900

    構文管理、ボタンの生成、名前変更まで出来るように。文字化けするので日本語フォントを導入する

--- NaegiofLegends1/SuperChatGame/Assets/Script/ButtonMake.cs
+++ NaegiofLegends1/SuperChatGame/Assets/Script/ButtonMake.cs
@@ -2,42 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonMake : MonoBehaviour
 {
     public GameObject parent;
     public KoubunManager koubunManager;
 
+    [SerializeField]
+    string[] koubunTest = new string[3];
+
     void Start()
     {
         parent = GameObject.Find("Text");
         koubunManager = gameObject.GetComponent<KoubunManager>();
-        StartCoroutine("ButtonChanging");
-    }
-
-    private void ButtonMaking()
-    {
-
-    }
-
-    IEnumerator ButtonChanging()
-    {
-        if(koubunManager.koubunArrays == null)
-        {
-            yield return null;
-        }
-
         GameObject button = (GameObject)Resources.Load("Button");
         for (int i = 0; i < 3; i++)
         {
             GameObject buttons = (GameObject)Instantiate(button, new Vector3(385, 120 - (i * 35), 0), Quaternion.identity);
             buttons.transform.SetParent(this.parent.transform, false);
-            /*
-            Text text = buttons.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>();
+            TextMeshProUGUI text = buttons.transform.Find("Text (TMP)").gameObject.GetComponent<TextMeshProUGUI>();
             text.text = koubunManager.koubunArrays[i].name;
-            */
-        }
 
+        }
+    }
+
+    private void ButtonMaking()
+    {
 
     }
 
--- NaegiofLegends1/SuperChatGame/Assets/Script/KoubunManager.cs
+++ NaegiofLegends1/SuperChatGame/Assets/Script/KoubunManager.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class Koubun
 {
     public string name; //構文の名前
     public string genre; //構文のジャンル。手にいれられる場所などに依存？
     public string naiyou; //構文の中身
+    /*
     public int ojisanPoint; //オジサンポイント
     public int kimoPoint; //気持ち悪さを見るキモポイント
     public int yabaiPoint;　//ヤバさをみるヤバポイント
+    */
 }
 
 public class KoubunManager : MonoBehaviour
 {
+    [SerializeField]
+    public List<Koubun> koubunLibrary = new List<Koubun>(); //構文用ソース
+
     [SerializeField]
     string[] koubunName = new string[6]; //構文名前入力欄
     [SerializeField]
     string[] koubunGenre = new string[6]; //構文genre入力欄
     [SerializeField]
     string[] koubunNaiyou = new string[6]; //構文内容入力欄
-    public Koubun[] koubunLibrary = new Koubun[6]; //構文用ソース
     [SerializeField]
     public Koubun[] koubunArrays = new Koubun[3]; //実際に選択された構文
 
     private void Start()
     {
-
-        for (int i = 0; i < 5; i++) //Libraryに構文を格納
+        for (int i = 0; i < 6; i++) //Libraryに構文を格納
         {
-            koubunLibrary[i].name = koubunName[i];
-            koubunLibrary[i].genre = koubunGenre[i];
-            koubunLibrary[i].naiyou = koubunNaiyou[i];
+            Debug.Log(koubunName[i]);
+            koubunLibrary.Add(new Koubun() {name = koubunName[i],genre = koubunGenre[i],naiyou = koubunNaiyou[i]});
+            Debug.Log(koubunLibrary[i].name);
         }
         KoubunGetRandom(0, 5, 3);
     }
@@ -47,7 +51,7 @@ public class KoubunManager : MonoBehaviour
             numbers.Add(i);
         }
 
-        for (int i = 1; i < count; i++)
+        for (int i = 0; i < count; i++)
         {
             int index = Random.Range(0, numbers.Count);
             int ransu = numbers[index];
