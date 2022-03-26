5404aaca8ee707961cc8b8fe8412e4dab96d3b04
rorisuto <yoko9973@gmail.com>
2022-03-26 18:36:31 +0900

    メッセージウィンドウの上でテキストを送る（クリックで）シーンが1つ出来たので、これを原型にしたいです（TwitchScene）

---
+++ NaegiofLegends1/SuperChatGame/Assets/Script/GameController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField]
+    Text scenarioMessage;
+    List<Scenario> scenarios = new List<Scenario>();
+
+    Scenario currentScenario;
+    int index = 0;
+
+    class Scenario
+    {
+        public string ScenarioID;
+        public List<string> Texts;
+        public List<string> Options;
+        public string NextScenarioID;
+    }
+
+    void Start()
+    {
+        var scenario01 = new Scenario()   //varはintとかのなんにでもなる、右辺によって変化する
+        {
+            ScenarioID = "scenario01",
+            Texts = new List<string>()
+            {
+                "テスト文章１",
+                "テスト文章２",
+                "テスト文章３",
+                "テスト文章４",
+                "テスト文章５",
+                "ぺコーラいつもありがとうを手に入れた"
+            }
+        };
+
+        SetScenario(scenario01);
+    }
+
+    void Update()
+    {
+        if (currentScenario != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SetNextMessage();
+            }
+        }
+    }
+
+    void SetScenario(Scenario scenario)
+    {
+        currentScenario = scenario;
+        scenarioMessage.text = currentScenario.Texts[0];
+    }
+
+    void SetNextMessage()
+    {
+        if (currentScenario.Texts.Count > index + 1)
+        {
+            index++;
+            scenarioMessage.text = currentScenario.Texts[index];
+        }
+        else
+        {
+            ExitScenario();
+        }
+    }
+
+    void ExitScenario()
+    {
+        scenarioMessage.text = "";
+        index = 0;
+        if (string.IsNullOrEmpty(currentScenario.NextScenarioID))
+        {
+            currentScenario = null;
+        }
+        else
+        {
+            var nextScenario = scenarios.Find
+                (s => s.ScenarioID == currentScenario.NextScenarioID);
+            currentScenario = nextScenario;
+        }
+    }
+
+}
