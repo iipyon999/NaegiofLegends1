120e7d640531d77ec6ae5ba77134b83bd119dc1e
rorisuto <yoko9973@gmail.com>
2022-03-26 16:06:40 +0900

    ゲーム開始画面のメニューを作りました

---
+++ NaegiofLegends1/SuperChatGame/Assets/Script/Menuselect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menuselect : MonoBehaviour
+{
+    public void StartGame(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
