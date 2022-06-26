cb7f63e4ea3e7f12640f06a6cbc4f228dcf0accd
IIPYON\kyota <kyota99930@yahoo.co.jp>
2022-06-26 15:36:51 +0900

    仮メインメニュー追加

---
+++ NaegiofLegends2/_tabi2/Assets/MetroidvaniaTools/scripts/Menu/EquipMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipMenu : MainMenu
+{
+    public GameObject obj;
+    public void EquipMenuMain()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            CloseEquipMenu();
+            OpenMenu();
+            menuupdate = opening;
+        }
+    }
+
+    public void OpenEquipMenu()
+    {
+        obj.SetActive(true);
+    }
+
+    public void CloseEquipMenu()
+    {
+        obj.SetActive(false);
+    }
+}
---
+++ NaegiofLegends2/_tabi2/Assets/MetroidvaniaTools/scripts/Menu/MainMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MainMenu : MonoBehaviour
+{
+    public delegate void MenuUpdate();
+    public MenuUpdate menuupdate;
+
+    [SerializeField]
+    public GameObject MainMenuObj;
+    [SerializeField]
+    public EquipMenu equipMenu;
+
+    private void Start()
+    {
+        menuupdate = closing;
+    }
+
+    private void Update()
+    {
+        menuupdate();
+    }
+
+    public void closing()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            OpenMenu();
+            menuupdate = opening;
+        }
+    }
+
+    public void opening()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            CloseMenu();
+            menuupdate = closing;
+        }
+    }
+
+    public void OpenMenu()
+    {
+        MainMenuObj.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        MainMenuObj.SetActive(false);
+    }
+    public void EquipMenuOpen()
+    {
+        CloseMenu();
+        equipMenu.OpenEquipMenu();
+        menuupdate = equipMenu.EquipMenuMain;
+    }
+}
