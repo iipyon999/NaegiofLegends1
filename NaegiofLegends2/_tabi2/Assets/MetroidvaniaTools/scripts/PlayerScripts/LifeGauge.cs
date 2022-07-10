61700e5e5bc1fa236d6436964680079823b76f18
rorisuto <yoko9973@gmail.com>
2022-07-10 18:06:49 +0900

    HPバーの追加

---
+++ NaegiofLegends2/_tabi2/Assets/MetroidvaniaTools/scripts/PlayerScripts/LifeGauge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeGauge : MonoBehaviour
+{
+    //　ライフゲージプレハブ
+    [SerializeField]
+    private GameObject lifeObj;
+
+    //　ライフゲージ全削除＆HP分作成
+    public void SetLifeGauge(int life)
+    {
+        //　体力を一旦全削除
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        //　現在の体力数分のライフゲージを作成
+        for (int i = 0; i < life; i++)
+        {
+            Instantiate<GameObject>(lifeObj, transform);
+        }
+    }
+}
---
+++ NaegiofLegends2/_tabi2/Assets/MetroidvaniaTools/scripts/PlayerScripts/LifeGaugeCharacter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeGaugeCharacter : MonoBehaviour
+{
+    //　HP
+    [SerializeField]
+    private int hp;
+    //　LifeGaugeスクリプト
+    [SerializeField]
+    private LifeGauge lifeGauge;
+    private LifeGaugeCharacter lifeGaugeCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //　体力の初期化
+        hp = 10;
+        //　体力ゲージに反映
+        var panels = GameObject.Find("Panel");
+        lifeGauge = panels.GetComponent<LifeGauge>();
+        lifeGauge.SetLifeGauge(hp);
+        lifeGaugeCharacter = this.GetComponent<LifeGaugeCharacter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)) 
+        {
+            Debug.Log("ダメージ");
+            lifeGaugeCharacter.Damage(1);
+        }
+    }
+
+    //　ダメージ処理メソッド（全削除＆HP分作成）
+    public void Damage(int damage)
+    {
+        hp -= damage;
+        //　0より下の数値にならないようにする
+        hp = Mathf.Max(0, hp);
+
+        if (hp >= 0)
+        {
+            lifeGauge.SetLifeGauge(hp);
+        }
+    }
+}
